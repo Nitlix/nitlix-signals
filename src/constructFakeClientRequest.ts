a528cb91d3c0f8a114b50ec69837f07243a67d78
Nit <66027553+Nitlix@users.noreply.github.com>
2023-11-29 14:58:09 +0000

    [/] No URL Requirement anyway

--- src/constructFakeClientRequest.ts
+++ src/constructFakeClientRequest.ts
@@ -1,15 +1,10 @@
 import { NextRequest } from "next/server";
-import { SignalCookie } from "./types";
-import getClientSignal from "./getClientSignal";
 import getAllClientSignals from "./getAllClientSignals";
 
-export default function(): NextRequest {
-    const url = getClientSignal("url") as (SignalCookie | undefined);
-    if (!url) throw new Error("No URL signal found. Seems like you're not using signalsInjector() in your middleware.")
+export default function (): NextRequest {
+    const request = new NextRequest("/")
 
-    const request = new NextRequest(url.value)
-
-    getAllClientSignals().forEach(({name, value})=>{
+    getAllClientSignals().forEach(({ name, value }) => {
         request.headers.set(name, value)
     })
 
